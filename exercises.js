00763c79c745e7dab4a84c6050c93c0a2102c49e
Alexandra Sirois <AlexandraSirois@Alexandras-MacBook-Pro.local>
2017-06-09 22:04:11 -1000

    completed #3

--- exercises.js
+++ exercises.js
@@ -68,7 +68,14 @@ console.log(login("test1234"));
  * Console.log your result.
 */
 
-
+function isGreaterThan(first, second){
+  if(first > second){
+    return true;
+  }else{
+    return false;
+  }
+}
+console.log(isGreaterThan(4,5));
 
 /*
  * #4
